d8e3642a8d5be5892d0ef6525f7cc39471ccea88
tkhv <harsha.02k@gmail.com>
2022-07-26 15:17:02 -0400

    Added users document

--- MongoDB/models/user.js
+++ MongoDB/models/user.js
@@ -1,7 +1,22 @@
+const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
+
+const ObjectId = mongodb.ObjectId;
+
 class User {
   constructor(username, email) {
     this.username = username;
     this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
+
+  static fetchById(userId) {
+    const db = getDb();
+    return db.collection("users").find({ _id: new ObjectId(userId) });
   }
 }
 
